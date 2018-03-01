cf87f823fd45cd5888d9835823e8d49ba9746552
ksha1018@naver.com <ksha1018@naver.com>
2018-03-01 06:56:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 chocolateBars.push("david kwon");
 chocolateBars.unshift("goowan kwon");
+chocolateBars =["sohyeon kwon", ...chocolateBars];
 console.log(chocolateBars);
