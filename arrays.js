68aa9c08ec037e7219f5407c6baca7949cc9d9a9
ksha1018@naver.com <ksha1018@naver.com>
2018-03-01 07:02:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,9 @@ chocolateBars.push("david kwon");
 chocolateBars.unshift("goowan kwon");
 chocolateBarsa =["sohyeon kwon", ...chocolateBars];
 console.log(chocolateBarsa);
+
+
+function addElementToBeginningOfArray(array, element){
+   var array1 = array.unshift(element);
+   return array1;
+}
